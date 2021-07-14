e678da29fc463fbfa9b80a5888bfc18164c8ac1b
Microsoft <82428818+River1997@users.noreply.github.com>
2021-07-14 09:22:29 +0800

    2021-07-14-小更新

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -20,6 +22,14 @@ class UserController extends Controller
         return view('admin.user.create');
     }
 
+    public function edit($id){
+
+        $record = User::find($id);
+
+        return view('admin.user.edit', compact('record'));
+
+    }
+
     public function store(Request $request){
 
         User::create([
@@ -29,5 +39,28 @@ class UserController extends Controller
         ]);
 
         return redirect('admin.user.index');
+    }
+
+    public function update(Request $request, $id){
+
+        $v = Validator::make($request->all(), [
+            // '欄位' '驗證規則'
+            'name' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+
+
+        if ($v->fails()){
+            return redirect()->back()->withErrors($v->errors());
+        }
+
+        $old_record = User::find($id);
+        $old_record ->name = $request ->name;
+        $old_record ->password = Hash::make($request ->password);
+        // $old_record ->role = $request ->role;
+        $old_record ->save();
+
+        return redirect('/admin/user')->with('message','成功');
     }
 }
---
+++ resources/views/admin/user/edit.blade.php
@@ -0,0 +1,137 @@
+@extends('layouts.template')
+
+
+@section('page_title')
+
+    編輯會員
+
+@endsection
+
+@section('css')
+
+@endsection
+
+
+@section('content')
+
+    <div class="container">
+
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{asset('/home')}}">首頁</a></li>
+                <li class="breadcrumb-item"><a href="{{asset('admin/user')}}">會員管理</a></li>
+                <li class="breadcrumb-item active" aria-current="page">編輯會員</li>
+            </ol>
+        </nav>
+
+
+
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>編輯會員</h2>
+                    </div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ asset('/admin/user/update') }}/{{ $record->id }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="role" class="col-md-4 col-form-label text-md-right">角色</label>
+
+                                <div class="col-md-3">
+                                    <select class="form-control" id="role" name="role">
+                                        <option @if ($record ->role == 'admin')selected                                       @endif>admin</option>
+                                        <option @if ($record ->role == 'user')selected                                       @endif>user</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $record->name }}" required autocomplete="name" autofocus>
+
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="phone" class="col-md-4 col-form-label text-md-right">電話</label>
+
+                                <div class="col-md-6">
+                                    <input id="phone" type="text" class="form-control" @if ($record->role == 'admin') disabled
+                                    @endif name="phone" value="{{ $record->client->phone??'' }}" required autocomplete="phone" autofocus>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="address" class="col-md-4 col-form-label text-md-right">地址</label>
+
+                                <div class="col-md-6">
+                                    <input id="address" type="address" class="form-control" @if ($record->role == 'admin') disabled
+                                    @endif name="address" value="{{ $record->client->address??'' }}" required autocomplete="address">
+
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                                <div class="col-md-6">
+
+                                    <input id="email" type="email" class="form-control "  value="{{ $record->email }}" autocomplete="email" readonly>
+                                    {{-- @error('email') is-invalid @enderror --}}
+
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        更新
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
+
+
+@section('js')
+
+@endsection
